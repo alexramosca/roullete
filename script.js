027826045acc92eb03aa8667294b7d886abda2a9
Alex Ramos <alex@ramos.com>
2023-03-01 09:33:20 -0400

    Added feature to select and unselect buttons and change their styles

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-class Button{
-    constructor(){
-        this.value = value;
-        this.probability = 2.7
-        this.selected = false;
-    }
-}
+
 
 $(document).ready(()=> {
+    class Button{
+        constructor(value){
+            this.value = value;
+            this.probability = 2.7
+            this.selected = false;
+        }
+
+        changeStatus(){
+            if(this.selected){
+                this.selected = false;
+                console.log("button desactivated")
+            }
+            else {this.selected = true; console.log("button activated")}
+        }
+    }
+
+    //Global list of buttons
+    let buttonsList = []
+
+
     for(let i = 0; i < 37; i++){
         let button = new Button(i)
         let buttonElement = document.createElement('button')
+        buttonsList.push(buttonElement)
         $(buttonElement).text(i)
+        $(buttonElement).addClass('btn btn-lg btn-outline-success playBtn')
+        $(buttonElement).id = `btn${i}`
         $('.col')[i].append(buttonElement);
+
+        //assign click event to each button
+       buttonsList[i].addEventListener('click', () => changeButton(button,i));
+      // buttonsList[i].addEventListener('click', ()=> )
+
+
     }
 
+    function changeButton(button,i){
+        console.log($('.playBtn')[i])
+        $(buttonsList[i]).toggleClass('btn-outline-success btn-success')
+        console.log(button.value)
+        button.changeStatus();
+        
+    }
 
-
-
-
-
+    
 
 
 
