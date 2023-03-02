f1562f04bb5f8f1bb44f929b563dae6db922833f
Alex Ramos <alex@ramos.com>
2023-03-02 08:43:17 -0400

    Added code to generate the bet, define if its the winner, calculate profit or loss and save at localstorage

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 $(document).ready(()=> {
+    class Bd {
+        saveBanking(winner, amount, odd){
+            let lastAmount = parseFloat(localStorage.getItem("banking"))
+            console.log(lastAmount)
+            let partialValue = 0;
+            if(lastAmount == undefined || isNaN(parseFloat(lastAmount))){
+                localStorage.setItem("banking", 1000)
+                lastAmount = parseFloat(1000);
+            }
+             alert(winner + " " + amount + " " + odd)
+            if(winner){
+                 partialValue = parseFloat(lastAmount + ((odd-1)*(50)))
+            }
+            else {
+                partialValue = lastAmount - amount
+            }
+            localStorage.setItem("banking", partialValue)
+            
+            
+        }
+    }
+    let bd = new Bd();
+    
+   
     class Bet{
         odd;
         constructor(amount){
-            this.amount = amount;
+            this.amount = parseFloat(amount);
             this.odd = 1;
             this.NumberWinner = 0;
             this.winner = false;
@@ -16,19 +40,20 @@ $(document).ready(()=> {
 
         //property to retrive the buttons selected and return the odd
         calculateOdd(){
+            console.log(this.generateRandom())
             let count = 0;
 
-            buttonInstances.forEach((b)=>{
+            buttonInstances.forEach((b)=>{ //It counts the numbers of buttons selected
                 if(b.selected) {
                     count++
                     this.numbers.push(b.value)
                 }
             })
             this.odd = 100/(buttonInstances[1].probability*count)
-            return (100/(buttonInstances[1].probability*count)).toFixed(2)
+            this.isWinner()
         }
 
-        //Define whether the bet is winner or not
+        //Define whether the bet is winner or not and call Bd class to save the changes
         isWinner(){
             let winner = false;
             this.numbers.forEach((n)=>{
@@ -36,7 +61,9 @@ $(document).ready(()=> {
                     winner = true;
                 }
             })
-            return winner
+            bd.saveBanking(winner, this.amount, this.odd)
+            
+            console.log(winner, this.amount, this.odd)
         } 
 
     }
@@ -87,23 +114,9 @@ $(document).ready(()=> {
     $("#btnPlay").click(makeBet)
 
     function makeBet(){
-        let bet = new Bet(0);
-        console.log(bet.calculateOdd())
-        console.log(bet.generateRandom())
-        console.log(bet.isWinner())
+        let bet = new Bet($("#txtBet").val())
+        bet.calculateOdd()
+        
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })
